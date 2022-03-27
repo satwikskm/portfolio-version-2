a314e9fb67fd3c9c470566b9e90c9fbd29b35442
S@TwIt <satwikmohantyskm@gmail.com>
2022-03-27 14:38:02 +0530

    version1

---
+++ script/script.js
@@ -0,0 +1,35 @@
+'use strict'
+let introButton = document.getElementById('info')
+let projectButton = document.getElementById('project')
+let resumeButton = document.getElementById('resume')
+let blogButton = document.getElementById('blog')
+let contactButton = document.getElementById('contact')
+let closeInfoButton=document.getElementById('close')
+let closeProjectButton=document.getElementById('project')
+
+introButton.addEventListener('click',()=>{
+    document.querySelector('.Infocontainer').classList.add('active')
+    document.querySelector('.skills').classList.add('active')
+    document.getElementById('info').style.color="blue"
+        document.getElementById('intro').style.display="block"
+
+    console.log("in")
+})
+
+closeInfoButton.addEventListener('click',()=>{
+    console.log(document.querySelector('.Infocontainer'))
+    document.querySelector('.Infocontainer').classList.remove('active')
+    document.querySelector('.skills').classList.remove('active')
+    document.getElementById('info').style.color="black"
+    document.getElementById('intro').style.display="none"
+})
+
+projectButton.addEventListener('click',()=>{
+    document.querySelector('.Infocontainer').classList.add('active')
+    document.querySelector('.skills').classList.add('active')
+    document.getElementById('project').style.color="blue"
+    document.getElementById('projects').style.display="block"
+
+    console.log("in")
+})
+
